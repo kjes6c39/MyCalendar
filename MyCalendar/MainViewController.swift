a19113e30ada815c43f0f36a070e8249f05ffee7
Eve Hsu <evehsu@E-MacBook-Pro.local>
2017-10-17 13:56:05 +0800

    new branch

---
+++ MyCalendar/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  MyCalendar
+//
+//  Created by Eve Hsu on 2017/10/17.
+//  Copyright © 2017年 Eve Hsu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Create New Branch")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
